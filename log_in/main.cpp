3a929b575e95404002cf82eac25fa4526a68dbd3
Matthew Walker <mwalker.za@gmail.com>
2021-04-30 17:37:26 +0200

    TODOs and some fixes

--- log_in/main.cpp
+++ log_in/main.cpp
@@ -18,7 +18,8 @@ int splashScreen() {
     cout << "         WEIRD HACKERMAN LOG IN SYSTEM         " << endl;
     cout << "***********************************************" << endl << endl;
     cout << "1: login" << endl;
-    cout << "2: register\n\n";
+    cout << "2: register" << endl;
+    cout << "3: exit\n\n";
     cout << "choice: ";
 
     cin >> choice;
@@ -29,6 +30,8 @@ int splashScreen() {
             break;
         case 2:
             registration();
+            break;
+        case 3:
             break;
         default:
             //TODO fix bug where if input isn't of type int causes infinite loop.
@@ -68,7 +71,7 @@ void registration() {
     cout.flush(); //Maybe turn this into a constructor when moving to class.
                   //TODO This needs fixing. It doesn't work. Needs to clear screen.
     string name, surname, email;
-    ofstream userFile("users.txt");
+    ofstream userFile("users.txt", std::ios_base::app);
 
     cout << "\n\nPlease enter your first name: ";
     cin >> name;
@@ -76,10 +79,15 @@ void registration() {
     cin >> surname;
     cout << "Please enter your email address: ";
     cin >> email;
-    userFile << name << ' ' << surname << ' ' << email;
+    userFile << surname << "," << name << ',' << email << endl;
 
     cout << "\nThank you, please check your mail and confirm registration.\n\n";
     //TODO implement a working email system????
     userFile.close();
     splashScreen();
 }
+
+//TODO function to ommit random characters from being added in registration.
+//TODO function to change lowercase to uppercase on first letters.
+//TODO function to change uppercase letters to lowercase in middle of names.
+//TODO function to restructure the user database into alphabetic order.
